238a94594e3de6f366ef58690e073ce3fedafc0e
kqiita <134604805+kqiita@users.noreply.github.com>
2024-08-21 15:20:17 +0900

    fix: change changelog-style,change works-style apps-style blogs-style

--- main.js
+++ main.js
@@ -18,11 +18,14 @@ for (var i in logs.reverse()) {
         return type;
     }
     const div = document.createElement("div");
-    div.id = "content" + String(i);
+    div.id = "log_content" + String(i);
+    div.style = "padding: 3px 0;margin: 2px 5px;";
     const p1 = document.createElement("p");
     p1.textContent = logs[i].year + "/" + logs[i].month + "/" + logs[i].day + ":" + henkan();
+    p1.style = "margin: 0 0;";
     const p2 = document.createElement("p");
     p2.textContent = logs[i].detail;
+    p2.style = "margin: 0 0;text-wrap: stable;";
     div.appendChild(p1);
     div.appendChild(p2);
     changelog_content.appendChild(div);
